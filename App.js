6a6e80881855f31fa0ca69cc5593cf1e1e26ac7d
AmandaFMX <amandafm87@gmail.com>
2022-09-07 23:00:49 -0300

    Atualização com yet

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Doe Rápido t</Text>
+      <Text>Doe Rápido</Text>
       <StatusBar style="auto" />
     </View>
   );
