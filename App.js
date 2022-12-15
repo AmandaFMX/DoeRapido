ceaf7243a423aa1ccaf9962bd796833e212d1696
Amanda Xavier <107374054+AmandaFMX@users.noreply.github.com>
2022-12-15 17:41:47 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,38 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import {Button, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from './views/Home';
-import Login from './views/Login';
-import Registro from './views/Registro';
-import Perfil from './views/Perfil';
+import { StatusBar } from 'react-native';
+import react from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import Routes from './src/routes'
+
 
 export default function App() {
 
-  const Stack = createStackNavigator();
-
   return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home}
-          options={{
-            headerShown:false
-          }} 
-          />
-          <Stack.Screen name="Login" component={Login}
-          options={{
-            headerShown:false
-          }}  />
-          <Stack.Screen name="Registro" component={Registro}
-          options={{
-            headerShown:false
-          }}  />
-          <Stack.Screen name="Perfil" component={Perfil}
-          options={{
-            headerShown:false
-          }}  />
-        </Stack.Navigator>
-      </NavigationContainer>
+   <NavigationContainer>
+    <StatusBar barStyle="dark-content"/>
+    <Routes/>
+   </NavigationContainer>
   );
-}
-
+};
